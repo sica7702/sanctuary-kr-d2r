23cb08e2e11f284d4793d88b8eaac7df6938dc26
sica7702 <wjsxnvoddl1@gmail.com>
2026-09-22 23:20:40 +0900

    feat: connect javascript model adapters

---
+++ javascript-model-adapters.mjs
@@ -0,0 +1,36 @@
+﻿export function createJavaScriptModelAdapters() {
+  return {
+    mcda: async (input) => {
+      const values = input.features.map(Number);
+      const score = values.length
+        ? values.reduce((sum, value) => sum + value, 0) / values.length
+        : 0;
+
+      return {
+        score,
+        method: "mcda-weighted-average",
+      };
+    },
+
+    apriori: async (input) => {
+      const transactions = input.marketContext?.transactions ?? [];
+
+      return {
+        score: transactions.length ? 1 : 0,
+        transactionCount: transactions.length,
+        method: "apriori-association-rules",
+      };
+    },
+
+    collaborative: async (input) => {
+      const recommendations =
+        input.marketContext?.recommendations ?? [];
+
+      return {
+        score: recommendations.length ? 1 : 0,
+        recommendationCount: recommendations.length,
+        method: "collaborative-filtering",
+      };
+    },
+  };
+}
